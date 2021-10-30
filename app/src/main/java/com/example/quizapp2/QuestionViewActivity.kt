0b068353c76d4ce22adcec8e6953da484803cce8
Alex Dixon <xandixon@gmail.com>
2021-10-30 15:06:23 -0400

    Mid video 207
    Programmatically change button styles, button not yet implemented.

--- app/src/main/java/com/example/quizapp2/QuestionViewActivity.kt
+++ app/src/main/java/com/example/quizapp2/QuestionViewActivity.kt
@@ -1,16 +1,24 @@
 package com.example.quizapp2
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import com.example.quizapp2.databinding.ActivityQuestionViewBinding
-//
 
-class QuestionViewActivity : AppCompatActivity() {
+
+class QuestionViewActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityQuestionViewBinding
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionsList:ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
+
     private var progressBar: ProgressBar? = null
     private var tvProgress: TextView? = null
     private var tvQuestion: TextView? = null
@@ -19,6 +27,7 @@ class QuestionViewActivity : AppCompatActivity() {
     private var tvOptionTwo: TextView? = null
     private var tvOptionThree: TextView? = null
     private var tvOptionFour: TextView? = null
+    private var btnSubmit: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,22 +43,47 @@ class QuestionViewActivity : AppCompatActivity() {
         tvOptionTwo = binding.optionTwo
         tvOptionThree = binding.optionThree
         tvOptionFour = binding.optionFour
+        btnSubmit = binding.btnSubmit
+
+        mQuestionsList = Constants.getQuestions()
+        setQuestion()
+        defaultOptionsView()
 
 
-        val questionsList = Constants.getQuestions()
-        Log.i("QuestionList size is","${questionsList.size}")
-        var currentPosition = 1
-        var cQuestion = questionsList[currentPosition -1]
+    }
+
+    private fun setQuestion() {
+        var cQuestion = mQuestionsList!![mCurrentPosition - 1]
 
         ivImage?.setImageResource(cQuestion.image)
-        progressBar?.progress = currentPosition
-        tvProgress?.text = "$currentPosition / ${progressBar?.max}"
+        progressBar?.progress = mCurrentPosition
+        tvProgress?.text = "$mCurrentPosition / ${progressBar?.max}"
         tvQuestion?.text = cQuestion.question
         tvOptionOne?.text = cQuestion.optionOne
         tvOptionTwo?.text = cQuestion.optionTwo
         tvOptionThree?.text = cQuestion.optionThree
         tvOptionFour?.text = cQuestion.optionFour
 
+        if(mCurrentPosition == mQuestionsList!!.size){
+            btnSubmit?.text = "Finish"
+        }else btnSubmit?.text = "Submit"
+    }
 
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>()
+//        options.add(tvOptionOne!!) //other way of doing it
+        tvOptionOne?.let{options.add(0,it)}
+        tvOptionTwo?.let{options.add(1,it)}
+        tvOptionThree?.let{options.add(2,it)}
+        tvOptionFour?.let{options.add(3,it)}
+
+        for (option in options){
+            option.setTextAppearance(R.style.text_button)
+            option.setBackgroundResource(R.drawable.default_text_outline) //this is required
+        }
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 }
